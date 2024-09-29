0d4fd3ea8775704819a6294cf848a34d3f2d3739
ThFate <30439572+ThFate07@users.noreply.github.com>
2024-09-29 15:43:43 +0530

    test

--- api/index.py
+++ api/index.py
@@ -206,4 +206,4 @@ def test():
     return jsonify({'message': 'API is working!'})
 
 if __name__ == '__main__':
-    app.run(port=5328, debug=True)
+    app.run(host='0.0.0.0', port=5328, debug=True)
